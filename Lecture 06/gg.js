e4425dec830e462c15c7e9c4a5b478a5ef66465d
song2000s@office.yuhan.ac.kr <song2000s@office.yuhan.ac.kr>
2022-04-10 03:00:19 +0900

    [ha] 6강 업로드

---
+++ Lecture 06/gg.js
@@ -0,0 +1,9 @@
+function ha() 
+{
+    document.getElementById("d0").innerHTML = "■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■";
+    document.getElementById("d1").innerHTML = "■ ■ ■ ■ ■ ■ ■ ■ ■";
+    document.getElementById("d2").innerHTML = "■ ■ ■ ■ ■ ■ ■";
+    document.getElementById("d3").innerHTML = "■ ■ ■ ■ ■";
+    document.getElementById("d4").innerHTML = "■ ■ ■";
+    document.getElementById("d5").innerHTML = "■";
+}
