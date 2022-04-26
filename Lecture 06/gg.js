4733e847e521e400340ffb10f9f8f81951c7d41b
song2000s@office.yuhan.ac.kr <song2000s@office.yuhan.ac.kr>
2022-04-27 03:57:58 +0900

    [ha]8주차 과제

--- Lecture 06/gg.js
+++ Lecture 06/gg.js
@@ -6,4 +6,24 @@ function ha()
     document.getElementById("d3").innerHTML = "■ ■ ■ ■ ■";
     document.getElementById("d4").innerHTML = "■ ■ ■";
     document.getElementById("d5").innerHTML = "■";
+
+    var array = [];
+
+    array.push("어렵네");
+    array.push("아");
+    array.push("제발");
+    array.push("으엥");
+    array.push("송하규");
+
+    //console.log(array.pop());
+   // console.log(array.pop());
+    //console.log(array.pop());
+   // console.log(array.pop());
+   // console.log(array.pop());
+
+    console.log(array.shift());
+    console.log(array.shift());
+    console.log(array.shift());
+    console.log(array.shift());
+    console.log(array.shift());
 }
